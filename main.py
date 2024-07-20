a71a62852ddf19993f232b127504b262c935aec1
Michael Bianco <iloveitaly@gmail.com>
2024-07-20 12:57:53 -0600

    it's working!

--- main.py
+++ main.py
@@ -1,72 +1,207 @@
-#!/usr/bin/python3
-
-# Copyright (C) 2023 Elliot Killick <contact@elliotkillick.com>
-# Licensed under the MIT License. See LICENSE file for details.
-
-"""
-Convert RSS to email newsletters and send them using the Listmonk API automatically
-
-On first run, we generate the feed_entry_links.txt file to start keeping track of new feed entries.
-
-On every run after that, we read feed_entry_links.txt checking for new entries since the previous
-read. If new entries are detected, we create and start a Listmonk campaign.
-
-We use absolute article links as the unique identifier for each entry.
-"""
-
-# pylint: disable=invalid-name
-
 import time
 import os
 from pathlib import Path
 import re
 from collections.abc import Iterator
+import backoff
 import requests
 import feedparser
 from lxml import etree
+from decouple import Csv, config
+import structlog
+
+# side effects! for append_text
+import listmonk_newsletter.pathlib_extension
+from listmonk_newsletter.util import configure_logger
+
+log = structlog.get_logger()
+configure_logger()
 
 PROGRAM_DIRECTORY = Path(__file__).parent.resolve()
 
 # Configuration variables
-ROOT_URL = "https://elliotonsecurity.com"
-RSS_URL = f"{ROOT_URL}/atom.xml"
+ROOT_URL = config("BLOG_URL", cast=str)
+RSS_URL = f"{ROOT_URL}/feed/"
 FEED_ENTRY_LINKS_FILE = PROGRAM_DIRECTORY / "feed_entry_links.txt"
-CONTENT_TEMPLATE_FILE = PROGRAM_DIRECTORY / "content_template.html"
-POLL_INTERVAL = 3 * 60  # Consider using a cron job or systemd timer instead
-DRY_RUN = False  # Use this for testing, campaigns will be created but no emails will be sent out
+CONTENT_TEMPLATE_FILE = PROGRAM_DIRECTORY / "template.j2"
 
-LISTMONK_URL = "https://ping.elliotonsecurity.com:8443"
-LISTMONK_USERNAME = "elliotkillick"
-LISTMONK_PASSWORD = "PASSWORD_HERE"
+DRY_RUN = True  # Use this for testing, campaigns will be created but no emails will be sent out
+
+LISTMONK_URL = config("LISTMONK_URL", cast=str)
+LISTMONK_USERNAME = config("LISTMONK_USERNAME", cast=str)
+LISTMONK_PASSWORD = config("LISTMONK_PASSWORD", cast=str)
+LISTMONK_TITLE = config("LISTMONK_TITLE", cast=str)
+
+LISTMONK_TEMPLATE = config("LISTMONK_TEMPLATE", cast=int, default=None)
+LISTMONK_SEND_AT = config("LISTMONK_SEND_AT", cast=str, default=None)
+LISTMONK_TEST_EMAILS = config("LISTMONK_TEST_EMAILS", cast=Csv(), default=None)
+
+BACKOFF_LIMIT = 1
+
+LISTMONK_REQUEST_PARAMS = {
+    "headers": {"Content-Type": "application/json;charset=utf-8"},
+    "auth": (LISTMONK_USERNAME, LISTMONK_PASSWORD),
+}
 
 
-def main():
-    """Program entry point"""
-
-    banner()
-
-    while True:
-        main_loop()
-        time.sleep(POLL_INTERVAL)
+def fetch_rss() -> feedparser.FeedParserDict | None:
+    feed = feedparser.parse(RSS_URL)
+    return feed
 
 
-def banner():
-    """Print program banner"""
+def populate_preexisting_entries(entry_links: list[str]) -> bool:
+    """Don't process feed entries that existed before first run of rss2newsletter"""
 
-    print(
-        "\n"
-        "               d88b                             8        w    w\n"
-        '8d8b d88b d88b " dP 8d8b. .d88b Yb  db  dP d88b 8 .d88b w8ww w8ww .d88b 8d8b\n'
-        "8P   `Yb. `Yb.  dP  8P Y8 8.dP'  YbdPYbdP  `Yb. 8 8.dP'  8    8   8.dP' 8P\n"
-        "8    Y88P Y88P d888 8   8 `Y88P   YP  YP   Y88P 8 `Y88P  Y8P  Y8P `Y88P 8\n"
-        "... by @ElliotKillick\n"
+    if not os.path.exists(FEED_ENTRY_LINKS_FILE):
+        log.info(
+            "Feed entry links file does not exist:"
+            f"{FEED_ENTRY_LINKS_FILE}. Populating it for the first time..."
+        )
+
+        FEED_ENTRY_LINKS_FILE.write_text("\n".join(entry_links), encoding="utf-8")
+        return True
+
+    log.info("Checking for new feed entries...")
+    return False
+
+
+def read_feed_entry_links_file() -> list[str]:
+    return FEED_ENTRY_LINKS_FILE.read_text(encoding="utf-8").strip("\n").split("\n")
+
+
+@backoff.on_exception(
+    backoff.expo,
+    # highest exception up the execption hierarchy
+    (requests.exceptions.RequestException),
+    max_tries=BACKOFF_LIMIT,
+)
+def get_og_image(url: str) -> str | None:
+    """Get Open Graph cover image URL from given HTML"""
+
+    html = requests.get(url).content
+
+    tree = etree.fromstring(html, etree.HTMLParser())
+
+    og_image = tree.find("head/meta[@property='og:image']")
+
+    if og_image is not None:
+        return og_image.get("content")
+
+
+import html2text
+
+import dateparser
+
+
+@backoff.on_exception(
+    backoff.expo,
+    # highest exception up the execption hierarchy
+    (requests.exceptions.RequestException),
+    max_tries=BACKOFF_LIMIT,
+)
+def create_campaign(title: str, body: str) -> int:
+    send_at = None
+
+    if LISTMONK_SEND_AT:
+        parsed_date = dateparser.parse(
+            LISTMONK_SEND_AT, settings={"PREFER_DATES_FROM": "future"}
+        )
+
+        # from listmonk docs:
+        # Timestamp to schedule campaign. Format: 'YYYY-MM-DDTHH:MM:SS'.
+
+        send_at = parsed_date.strftime("%Y-%m-%dT%H:%M:%SZ")
+
+    # https://listmonk.app/docs/apis/campaigns/#post-apicampaigns
+    json_data = {
+        "name": title,
+        "subject": title,
+        "lists": [1],
+        "content_type": "html",
+        "body": body,
+        "send_at": send_at,
+        "altbody": html2text.html2text(body),
+        "template_id": LISTMONK_TEMPLATE,
+        "messenger": "email",
+        "type": "regular",
+        "tags": ["listmonk-newsletter"],
+    }
+
+    response = requests.post(
+        f"{LISTMONK_URL}/api/campaigns", json=json_data, **LISTMONK_REQUEST_PARAMS
     )
 
+    return response.json()["data"]["id"]
 
-def main_loop():
-    """Program infinite loop"""
 
+# TODO this is broken on the listmonk side of things
+@backoff.on_exception(
+    backoff.expo,
+    # highest exception up the execption hierarchy
+    (requests.exceptions.RequestException),
+    max_tries=BACKOFF_LIMIT,
+)
+def send_tests(campaign_id: int, emails: list[str]):
+    """
+    Note that all emails must be in the list attached to the campaign, which is a weird requirement
+    """
+    response = requests.post(
+        f"{LISTMONK_URL}/api/campaigns/{campaign_id}/test",
+        json={
+            "subscribers": emails,
+        },
+        **LISTMONK_REQUEST_PARAMS,
+    )
+
+    response.raise_for_status()
+
+
+@backoff.on_exception(
+    backoff.expo,
+    # highest exception up the execption hierarchy
+    (requests.exceptions.RequestException),
+    max_tries=BACKOFF_LIMIT,
+)
+def start_campaign(campaign_id: int) -> bool:
+    response = requests.put(
+        f"{LISTMONK_URL}/api/campaigns/{campaign_id}/status",
+        json={"status": "scheduled"},
+        **LISTMONK_REQUEST_PARAMS,
+    )
+
+    response.raise_for_status()
+
+    return response.status_code == 200
+
+
+import jinja2
+
+
+import css_inline
+
+
+def render_email_content(new_entries: list[feedparser.FeedParserDict]) -> str:
+    # Create a Jinja2 environment and load the template file
+    env = jinja2.Environment(
+        loader=jinja2.FileSystemLoader(searchpath=str(PROGRAM_DIRECTORY)),
+        autoescape=jinja2.select_autoescape(["html", "xml"]),
+    )
+
+    template = env.get_template(str("template.j2"))
+    rendered_content = template.render(entries=new_entries)
+
+    inliner = css_inline.CSSInliner(keep_style_tags=True)
+    inlined_content = inliner.inline(rendered_content)
+
+    return inlined_content
+
+
+import funcy_pipe as fp
+
+
+def generate_campaign():
     feed = fetch_rss()
+
     if not feed:
         return
 
@@ -75,201 +210,42 @@ def main_loop():
 
     # On first run (feed_entry_links.txt does not exist)
     if populate_preexisting_entries((e.link for e in feed.entries)):
+        log.info("first run, assuming all entries are new")
         return
 
     entry_links_last_update = read_feed_entry_links_file()
-    for new_entry in check_for_new_entries(entry_links_last_update, feed.entries):
-        campaign_id = create_newsletter(new_entry.link, new_entry.title)
-        send_successful = send_newsletter(campaign_id)
-        if send_successful:
-            update_feed_entry_links_file(new_entry.link)
 
+    def add_image_link(entry):
+        og_image = get_og_image(entry.link)
+        entry["image"] = og_image
+        return entry
 
-def fetch_rss() -> feedparser.FeedParserDict | None:
-    """Fetch and parse RSS"""
+    new_entries = (
+        feed.entries
+        | fp.filter(lambda e: e.link not in entry_links_last_update)
+        | fp.map(add_image_link)
+        | fp.to_list()
+    )
 
-    feed = feedparser.parse(RSS_URL)
-    # In case of failure
-    if hasattr(feed, "bozo_exception"):
-        print(f"Error fetching RSS from: {RSS_URL}")
-        return None
+    if not new_entries:
+        log.info("no new entries found")
+        return
 
-    return feed
+    content = render_email_content(new_entries)
+    campaign_id = create_campaign(LISTMONK_TITLE, content)
 
+    send_successful = start_campaign(campaign_id)
 
-def populate_preexisting_entries(entry_links: list[str]) -> bool:
-    """Don't process feed entries that existed before first run of rss2newsletter"""
+    if send_successful:
+        # TODO this is currently broken on the listmonk side
+        # if LISTMONK_TEST_EMAILS:
+        #     send_tests(campaign_id, LISTMONK_TEST_EMAILS)
 
-    if not os.path.exists(FEED_ENTRY_LINKS_FILE):
-        print(
-            "Feed entry links file does not exist:"
-            f"{FEED_ENTRY_LINKS_FILE}. Populating it for the first time..."
-        )
-        create_feed_entry_links_file(entry_links)
-        return True
-
-    print("Checking for new feed entries...")
-    return False
-
-
-def create_feed_entry_links_file(entry_links: list[str]):
-    """Create initial feed entry links file"""
-
-    with open(FEED_ENTRY_LINKS_FILE, "w", encoding="utf-8") as f:
-        f.write("\n".join(entry_links))
-
-
-def update_feed_entry_links_file(entry_link: str):
-    """Update feed entry links file by appending so new entries are no longer considered new"""
-
-    with open(FEED_ENTRY_LINKS_FILE, "a", encoding="utf-8") as f:
-        f.write(f"{entry_link}\n")
-
-
-def read_feed_entry_links_file() -> list[str]:
-    """Read state of which feed entries need no further processing"""
-
-    with open(FEED_ENTRY_LINKS_FILE, "r", encoding="utf-8") as f:
-        return f.read().strip("\n").split("\n")
-
-
-def check_for_new_entries(
-    entry_links_last_update: list[str], entries: list[feedparser.FeedParserDict]
-) -> Iterator[feedparser.FeedParserDict]:
-    """Iterate feed entries looking for any new ones"""
-
-    for entry in entries:
-        if entry.link not in entry_links_last_update:
-            yield entry
-
-
-def create_newsletter(link: str, title: str) -> int | None:
-    """Create newsletter with content and add campaign to Listmonk"""
-
-    print("Creating newsletter for:", title)
-    return create_campaign(title, create_content(link, title))
-
-
-def create_content(link: str, title: str) -> str:
-    """Create content to be used as body of newsletter"""
-
-    with open(CONTENT_TEMPLATE_FILE, "r", encoding="utf-8") as f:
-        content = f.read()
-
-    content = content.replace("LINK_HERE", link)
-    content = content.replace("TITLE_HERE", title)
-
-    og_image = get_og_image(fetch_url(link))
-    if og_image:
-        content = content.replace("IMAGE_HERE", og_image)
-    else:
-        # Remove optional image section
-        content = re.sub(
-            "IMAGE_OPTIONAL_BEGIN.*IMAGE_OPTIONAL_END\n", "", content, flags=re.DOTALL
-        )
-
-    return content
-
-
-def fetch_url(url: str) -> str | None:
-    """Get response body at a URL"""
-
-    while True:
-        try:
-            return requests.get(url).content
-        except requests.exceptions.ConnectionError:
-            print(f"Failed to fetch URL: {url}! Retrying in 60 seconds...")
-            time.sleep(60)
-
-
-def get_og_image(html: str) -> str | None:
-    """Get Open Graph cover image URL from given HTML"""
-
-    # I've confirmed (by testing with a payload) that HTMLParser is NOT vulnerable to XXE
-    # https://bugs.launchpad.net/lxml/+bug/1742885
-    # https://lxml.de/4.0/api/lxml.etree.HTMLParser-class.html
-    tree = etree.fromstring(html, etree.HTMLParser())
-
-    og_image = tree.find("head/meta[@property='og:image']")
-    if og_image:
-        return og_image.get("content")
-
-
-def send_newsletter(campaign_id: int) -> bool:
-    """Given a campaign ID tell Listmonk to start it then perform error handling"""
-
-    if DRY_RUN:
-        print("Dry run: Not sending newsletter")
-        return True
-
-    print("Sending newsletter...")
-    status_code = start_campaign(campaign_id)
-    if status_code == 200:
-        print("Successfully started campaign!")
-        return True
-
-    print("Error starting campaign!")
-    return False
-
-
-def create_campaign(title: str, body: str) -> int:
-    """Create Listmonk email campaign for new article"""
-
-    headers = {"Content-Type": "application/json;charset=utf-8"}
-
-    json_data = {
-        "name": f"New Article: {title}",
-        "subject": title,
-        "lists": [1],  # List ID 1 is my "New Articles" list
-        "content_type": "richtext",
-        "body": body,
-        "messenger": "email",
-        "type": "regular",
-        "tags": ["new-article"],
-    }
-
-    while True:
-        try:
-            response = requests.post(
-                f"{LISTMONK_URL}/api/campaigns",
-                headers=headers,
-                json=json_data,
-                auth=(LISTMONK_USERNAME, LISTMONK_PASSWORD),
-            )
-            break
-        except requests.exceptions.ConnectionError:
-            print(
-                "Failed to send Listmonk campaign create request! Retrying in 60 seconds..."
-            )
-            time.sleep(60)
-
-    return response.json()["data"]["id"]
-
-
-def start_campaign(campaign_id: int) -> int:
-    """Start Listmonk email campaign"""
-
-    headers = {"Content-Type": "application/json"}
-
-    json_data = {"status": "running"}
-
-    while True:
-        try:
-            response = requests.put(
-                f"{LISTMONK_URL}/api/campaigns/{campaign_id}/status",
-                headers=headers,
-                json=json_data,
-                auth=(LISTMONK_USERNAME, LISTMONK_PASSWORD),
-            )
-            break
-        except requests.exceptions.ConnectionError:
-            print(
-                "Failed to send Listmonk campaign start request! Retrying in 60 seconds..."
-            )
-            time.sleep(60)
-
-    return response.status_code
+        # TODO should really have a `exec()` or something in fp for this use case
+        new_entries | fp.pluck_attr("link") | fp.map(lambda t: "\n" + t) | fp.map(
+            FEED_ENTRY_LINKS_FILE.append_text
+        ) | fp.to_list()
 
 
 if __name__ == "__main__":
-    main()
+    generate_campaign()
